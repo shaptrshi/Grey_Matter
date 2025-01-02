f5714220f83d830e99819c4739928ad134a161c5
shaptrshi <59893262+shaptrshi@users.noreply.github.com>
2025-01-02 21:48:05 +0530

    second commit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Navbar/>
   )
 }
 
---
+++ front-end/src/components/navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="shadow-lg">
+      <div className="container mx-auto flex items-center justify-between px-10 py-5">
+        {/* Logo Section */}
+        <div className="flex items-center space-x-4">
+          <img
+            src="./logo.svg"
+            alt="Logo"
+            className=" border-gray-700 cursor-pointer"
+          />
+        </div>
+
+
+        <div className="hidden md:flex items-center space-x-8 text-sm font-medium">
+          <a
+            href="#featured"
+            className="text-gray-800 hover:text-gray-400 transition-colors duration-200 text-xl"
+          >
+            Featured
+          </a>
+          <a
+            href="#news"
+            className="text-gray-800 hover:text-gray-400 transition-colors duration-200 cursor-pointer text-xl"
+          >
+            News
+          </a>
+          <a
+            href="#science"
+            className="text-gray-800 hover:text-gray-400 transition-colors duration-200 cursor-pointer text-xl"
+          >
+            Science
+          </a>
+          <a
+            href="#technology"
+            className="text-gray-800 hover:text-gray-400 transition-colors duration-200 cursor-pointer text-xl"
+          >
+            Technology
+          </a>
+        </div>
+
+        {/* Search Bar */}
+        <div className="flex items-center bg-gray-200 rounded-lg overflow-hidden hover:bg-gray-300 transition-all duration-200 hover:shadow-lg">
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent flex-1 px-4 py-2 text-black placeholder-gray-500 focus:outline-none"
+          />
+          <button className=" p-3 flex items-center justify-center">
+            <img src="./search.png" alt="" className="w-5 h-5"/>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
