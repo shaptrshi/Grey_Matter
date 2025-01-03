47313927fc84b0b749ce59068727830516564622
shaptrshi <59893262+shaptrshi@users.noreply.github.com>
2025-01-03 17:44:23 +0530

    dd

--- front-end/src/components/footer/Footer.jsx
+++ front-end/src/components/footer/Footer.jsx
@@ -6,11 +6,13 @@ const Footer = () => {
     <footer className="bg-white py-10">
       <div className="container mx-auto text-center space-y-7">
         <div>
+          <Link to="/">  
           <img
             src="./logo.svg"
             alt="Logo"
-            className="w-50 h-50 mx-auto transition-transform transform hover:scale-105"
+            className="w-50 h-50 mx-auto transition-transform transform hover:scale-105 cursor-pointer"
           />
+          </Link>
         </div>
 
         <div className="flex justify-center space-x-8 text-gray-700 text-xl font-medium">
