b16d76d72e43a0770a6e0050a8b55c6e9566d317
shaptrshi <59893262+shaptrshi@users.noreply.github.com>
2025-05-14 00:47:11 +0530

    logo change

--- front-end/src/components/footer/Footer.jsx
+++ front-end/src/components/footer/Footer.jsx
@@ -1,52 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo2.png";
+import logo from "../../assets/logo.svg";
 
 const Footer = () => {
   return (
-    <footer className="bg-white dark:bg-custom-dark py-6 sm:py-8 md:py-10 mt-6 sm:mt-8 md:mt-10 border-t border-gray-300 dark:border-black shadow-md">
-      <div className="container mx-auto px-4 text-center space-y-4 sm:space-y-6 md:space-y-7">
+    <footer className="bg-white dark:bg-custom-dark py-3 sm:py-4 md:py-6 mt-4 sm:mt-6 md:mt-8 border-t border-gray-300 dark:border-black shadow-md">
+      <div className="container mx-auto px-4 text-center space-y-3 sm:space-y-4 md:space-y-6">
         {/* Logo Section */}
-        <div className="w-full max-w-[200px] mx-auto">
+        <div className="w-full max-w-[300px] mx-auto mb-3 sm:mb-4 md:mb-6">
           <Link to="/">
             <img
-              src= {logo}
+              src={logo}
               alt="Logo"
-              className="w-32 h-16 sm:w-40 sm:h-16 md:w-50 md:h-20 mx-auto transition-transform transform hover:scale-105 cursor-pointer object-contain"
+              className="w-112 h-64 sm:w-128 sm:h-72 md:w-140 md:h-80 mx-auto transition-transform transform hover:scale-105 cursor-pointer object-contain"
             />
           </Link>
         </div>
 
         {/* Navigation Links */}
-        <nav className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-4 md:space-x-8">
-        <Link
+        <nav className="flex flex-col sm:flex-row justify-center items-center space-y-3 sm:space-y-0 sm:space-x-4 md:space-x-6">
+          <Link
             to="/aboutus"
-            className="group relative cursor-pointer transition-colors duration-300 dark:hover:text-custom-green hover:text-custom-green text-gray-700 dark:text-gray-100 text-base sm:text-lg md:text-xl font-medium"
+            className="group relative cursor-pointer transition-colors duration-300 dark:hover:text-custom-green hover:text-custom-green text-gray-700 dark:text-gray-100 text-sm sm:text-base md:text-lg font-medium"
           >
             About Us
           </Link>
           <Link
             to="/privacy-policy"
-            className="group relative cursor-pointer transition-colors duration-300 dark:hover:text-custom-green hover:text-custom-green text-gray-700 dark:text-gray-100 text-base sm:text-lg md:text-xl font-medium"
+            className="group relative cursor-pointer transition-colors duration-300 dark:hover:text-custom-green hover:text-custom-green text-gray-700 dark:text-gray-100 text-sm sm:text-base md:text-lg font-medium"
           >
             Privacy Policy
           </Link>
           <Link
             to="/terms-of-use"
-            className="group relative cursor-pointer transition-colors duration-300 dark:hover:text-custom-green hover:text-custom-green text-gray-700 dark:text-gray-100 text-base sm:text-lg md:text-xl font-medium"
+            className="group relative cursor-pointer transition-colors duration-300 dark:hover:text-custom-green hover:text-custom-green text-gray-700 dark:text-gray-100 text-sm sm:text-base md:text-lg font-medium"
           >
             Terms of Use
           </Link>
           <Link
             to="/contact"
-            className="group relative cursor-pointer transition-colors duration-300 dark:hover:text-custom-green hover:text-custom-green text-gray-700 dark:text-gray-100 text-base sm:text-lg md:text-xl font-medium"
+            className="group relative cursor-pointer transition-colors duration-300 dark:hover:text-custom-green hover:text-custom-green text-gray-700 dark:text-gray-100 text-sm sm:text-base md:text-lg font-medium"
           >
             Contact
           </Link>
         </nav>
 
         {/* Copyright Text */}
-        <p className="text-gray-700 text-center dark:text-gray-100 text-sm">&copy; {new Date().getFullYear()} Grey Matter. All rights reserved.</p>
+        <p className="text-gray-700 text-center dark:text-gray-100 text-xs sm:text-sm md:text-base">
+          &copy; {new Date().getFullYear()} that grey matter. All rights reserved.
+        </p>
       </div>
     </footer>
   );
