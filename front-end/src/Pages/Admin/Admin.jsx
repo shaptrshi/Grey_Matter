ea20e2440114d437e74cf2ac73ad919f37d84522
shaptrshi <59893262+shaptrshi@users.noreply.github.com>
2025-05-30 14:21:39 +0530

    search

--- front-end/src/Pages/Admin/Admin.jsx
+++ front-end/src/Pages/Admin/Admin.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Tooltip, TooltipProvider } from "@/components/ui/tooltip";
-import { FaHome, FaUser, FaSignOutAlt } from "react-icons/fa"; 
+import { FaHome, FaUser, FaSignOutAlt } from "react-icons/fa";
 import axios from "axios";
 import {
   Card,
@@ -16,7 +16,6 @@ import SearchBar from "../../components/searchbar/searchBar";
 
 const Admin = () => {
   const [authors, setAuthors] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
 
   const navigate = useNavigate();
 
@@ -108,7 +107,6 @@ const Admin = () => {
     }
   };
 
-
   const handleGoToHomePage = () => navigate("/");
 
   const handleGoToProfilePage = (authorId) => {
@@ -158,86 +156,78 @@ const Admin = () => {
       {/* Main Content */}
       <div className="container mx-auto px-10 py-2 bg-gray-100 dark:bg-custom-dark">
         {/* Authors & Articles */}
-        {authors
-          .filter(
-            (author) =>
-              author.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-              author.articles.some((article) =>
-                article.title.toLowerCase().includes(searchTerm.toLowerCase())
-              )
-          )
-          .map((author) => (
-            <div key={author.id} className="mb-10">
-              <div className="flex items-center space-x-4">
-                <img
-                  src={author.profilePicture}
-                  alt={author.name}
-                  className="w-16 h-16 rounded-full"
-                />
-                <h2 className="text-xl font-semibold">{author.name}</h2>
+        {authors.map((author) => (
+          <div key={author.id} className="mb-10">
+            <div className="flex items-center space-x-4">
+              <img
+                src={author.profilePicture}
+                alt={author.name}
+                className="w-16 h-16 rounded-full"
+              />
+              <h2 className="text-xl font-semibold">{author.name}</h2>
 
-                {/* Author Actions */}
-                <div className="ml-auto space-x-3">
-                  <Button
-                    onClick={() => handleGoToProfilePage(author.id)}
-                    className="text-gray-800 dark:text-white dark:bg-custom-dark bg-gray-100 px-3 rounded-lg hover:bg-blue-400 dark:hover:bg-blue-600 shadow-none"
-                  >
-                    <FaUser size={24} />
-                  </Button>
+              {/* Author Actions */}
+              <div className="ml-auto space-x-3">
+                <Button
+                  onClick={() => handleGoToProfilePage(author.id)}
+                  className="text-gray-800 dark:text-white dark:bg-custom-dark bg-gray-100 px-3 rounded-lg hover:bg-blue-400 dark:hover:bg-blue-600 shadow-none"
+                >
+                  <FaUser size={24} />
+                </Button>
 
-                  <Button
-                    onClick={() => handleDeleteAuthor(author.id)}
-                    className="text-red-700  dark:text-red-600 py-1 dark:bg-custom-dark bg-gray-100 px-3 rounded-lg dark:hover:bg-red-400 hover:bg-red-300 hover:text-gray-800 shadow-none"
-                  >
-                    Delete
-                  </Button>
-                </div>
-              </div>
-
-              {/* Articles */}
-              <div className="container mx-auto px-6 py-6">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
-                  {author.articles.map((article) => (
-                    <Link to={article.link} key={article.id} className="block">
-                      <Card className="hover:shadow-md transition-transform transform hover:scale-105 p-2 h-[280px] sm:h-[300px] dark:bg-custom-dark dark:border-none dark:shadow-sm dark:shadow-black">
-                        {/* Fixed card height */}
-                        <div className="relative h-[150px] sm:h-[150px]">
-                          <img
-                            src={article.bannerImage}
-                            alt={article.title}
-                            className="absolute inset-0 w-full h-full object-cover rounded-t-lg"
-                          />
-                        </div>
-                        <CardHeader className="p-3 sm:p-4 mt-1">
-                          <CardTitle className="text-lg sm:text-lg font-semibold text-gray-800 line-clamp-2 hover:underline dark:text-gray-100">
-                            {article.title}
-                          </CardTitle>
-                        </CardHeader>
-                        <CardContent className="p-3 sm:p-4 -mt-2">
-                          <p className="font-semibold text-teal-700">
-                            {article.date}
-                          </p>
-                          <div className="flex justify-end gap-4 mt-2">
-                            <Button
-                              onClick={(e) => {
-                                e.preventDefault();
-                                handleDelete(article.id);
-                              }}
-                              variant="destructive"
-                              size="sm"
-                              className="dark:text-white dark:bg-red-700"
-                            >
-                              Delete
-                            </Button>
-                          </div>
-                        </CardContent>
-                      </Card>
-                    </Link>
-                  ))}
-                </div>
+                <Button
+                  onClick={() => handleDeleteAuthor(author.id)}
+                  className="text-red-700  dark:text-red-600 py-1 dark:bg-custom-dark bg-gray-100 px-3 rounded-lg dark:hover:bg-red-400 hover:bg-red-300 hover:text-gray-800 shadow-none"
+                >
+                  Delete
+                </Button>
               </div>
             </div>
-          ))}
+
+            {/* Articles */}
+            <div className="container mx-auto px-6 py-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
+                {author.articles.map((article) => (
+                  <Link to={article.link} key={article.id} className="block">
+                    <Card className="hover:shadow-md transition-transform transform hover:scale-105 p-2 h-[280px] sm:h-[300px] dark:bg-custom-dark dark:border-none dark:shadow-sm dark:shadow-black">
+                      {/* Fixed card height */}
+                      <div className="relative h-[150px] sm:h-[150px]">
+                        <img
+                          src={article.bannerImage}
+                          alt={article.title}
+                          className="absolute inset-0 w-full h-full object-cover rounded-t-lg"
+                        />
+                      </div>
+                      <CardHeader className="p-3 sm:p-4 mt-1">
+                        <CardTitle className="text-lg sm:text-lg font-semibold text-gray-800 line-clamp-2 hover:underline dark:text-gray-100">
+                          {article.title}
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent className="p-3 sm:p-4 -mt-2">
+                        <p className="font-semibold text-teal-700">
+                          {article.date}
+                        </p>
+                        <div className="flex justify-end gap-4 mt-2">
+                          <Button
+                            onClick={(e) => {
+                              e.preventDefault();
+                              handleDelete(article.id);
+                            }}
+                            variant="destructive"
+                            size="sm"
+                            className="dark:text-white dark:bg-red-700"
+                          >
+                            Delete
+                          </Button>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
